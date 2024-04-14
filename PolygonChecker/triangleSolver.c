8a6c33680e3d06e602aedeb35bbaa8b12f7436f3
Vichu2005g <saygg.thebigamer@gmail.com>
2024-04-14 05:31:37 -0400

    Made changes to analyzeTriangle where instead of now printing the angles explictly, the angles are stored in a premade array (that must be passed when invoking) which holds all the angles. Due to the changes, I have also modified analyzeTriangle by creating a new array variable and using for loop to print out each angle.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -11,7 +11,7 @@ float radiansToDegrees(float radians)
 	return radians * (180.0 / PI);
 }
 
-void getAngles(float a, float b, float c)
+void getAngles(float a, float b, float c, float arr[])
 {
 	float angle_a = acos((b * b + c * c - a * a) / (2 * b * c));
 	angle_a = radiansToDegrees(angle_a);
@@ -22,27 +22,39 @@ void getAngles(float a, float b, float c)
 	float angle_c = acos((a * a + b * b - c * c) / (2 * a * b));
 	angle_c = radiansToDegrees(angle_c);
 
-	printf("The angles are %f , %f , %f\n", angle_a, angle_b, angle_c);
+	arr[0] = angle_a;
+	arr[1] = angle_b;
+	arr[2] = angle_c;
 }
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = ""; 
+	float angles[3];
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0 || 
 		!(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)) {
 		result = "Not a triangle";
 	}
 	else if (side1 == side2 && side1 == side3) {
-		getAngles(side1, side2, side3);
+		getAngles(side1, side2, side3, angles);
+		for (int i = 0; i < 3; i++)
+			printf(" %f", angles[i]);
+
 		result = "Equilateral triangle";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
 		(side1 == side3 && side1 != side2))
 	{
-		getAngles(side1, side2, side3);
+		getAngles(side1, side2, side3, angles);
+		for (int i = 0; i < 3; i++)
+			printf(" %f", angles[i]);
+
 		result = "Isosceles triangle";
 	}
 	else {
-		getAngles(side1, side2, side3);
+		getAngles(side1, side2, side3, angles);
+		for (int i = 0; i < 3; i++)
+			printf(" %f", angles[i]);
+
 		result = "Scalene triangle";
 	}
 
