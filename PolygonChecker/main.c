38623da11a667d7001eb24ee1dd4422e03044803
Vichu2005g <saygg.thebigamer@gmail.com>
2024-04-15 15:42:26 -0400

    added some indentation to make the code more readable

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -21,9 +21,11 @@ int main() {
 		case '1':
 		{
 			printf_s("Triangle selected.\n");
+
 			int triangleSides[3];
 			int* triangleSidesPtr = getTriangleSides(triangleSides);
 			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
+
 			printf_s("%s\n", result);
 			break;
 		}
