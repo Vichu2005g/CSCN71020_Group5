4ecefe0002c1f3de8beb803043fec17ec2d71088
Vichu2005g <saygg.thebigamer@gmail.com>
2024-04-15 15:06:39 -0400

    Removed that one comment in case 1 and removed the unnecessary values assigned in triangleSides variable

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -21,9 +21,8 @@ int main() {
 		case '1':
 		{
 			printf_s("Triangle selected.\n");
-			int triangleSides[3] = { 0, 0, 0 };
+			int triangleSides[3];
 			int* triangleSidesPtr = getTriangleSides(triangleSides);
-			//printf_s("! %d\n", triangleSidesPtr[0]);
 			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
 			printf_s("%s\n", result);
 			break;
